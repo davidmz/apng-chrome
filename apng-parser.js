e6e106f9f68e5afe23b79c4a51846445a4164f50
David Mzareulyan <david@hiero.ru>
2011-06-26 14:34:51 +0400

    Начальный коммит

---
+++ apng-parser.js
@@ -0,0 +1,178 @@
+(function() {
+    var global = (function() {
+        return this;
+    })(), D = Deferred;
+
+    var PNG_SIGNATURE_BYTES = new Uint8Array(new ArrayBuffer(8));
+    PNG_SIGNATURE_BYTES.set([0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a]);
+
+    var BlobBuilder = (global.BlobBuilder || global.WebKitBlobBuilder);
+    var URL = (global.URL || global.webkitURL);
+
+    var readDWord = function(bytes, off) {
+        var x = 0;
+        for (var i = 0; i < 4; i++) x += (bytes[i + off] << ((3 - i) * 8));
+        return x;
+    };
+    var readWord = function(bytes, off) {
+        var x = 0;
+        for (var i = 0; i < 2; i++) x += (bytes[i + off] << ((1 - i) * 8));
+        return x;
+    };
+    var readByte = function(bytes, off) {
+        return bytes[off];
+    };
+    var readString = function(bytes, off, length) {
+        var chars = Array.prototype.slice.call(bytes.subarray(off, off + length));
+        return String.fromCharCode.apply(String, chars);
+    };
+
+    var makeDWordArray = function(x) {
+        return [(x >> 24) & 0xff, (x >> 16) & 0xff, (x >> 8) & 0xff, x & 0xff];
+    };
+    var makeStringArray = function(x) {
+        var res = [];
+        for (var i = 0; i < x.length; i++) res.push(x.charCodeAt(i));
+        return res;
+    };
+    var makeChunkBytes = function(/* string */ type, /* bytes */ dataBytes) {
+        var crcLen = type.length + dataBytes.length;
+        var bytes = new Uint8Array(new ArrayBuffer(crcLen + 8));
+        bytes.set(makeDWordArray(dataBytes.length), 0);
+        bytes.set(makeStringArray(type), 4);
+        bytes.set(dataBytes, 8);
+        var crc = crc32(bytes, 4, crcLen);
+        bytes.set(makeDWordArray(crc), crcLen + 4);
+        return bytes;
+    };
+
+    var subBuffer = function(bytes, start, length) {
+        var a = new Uint8Array(new ArrayBuffer(length));
+        a.set(bytes.subarray(start, start + length));
+        return a.buffer;
+    };
+
+
+    var parseChunks = function(bytes, callback) {
+        var off = 8;
+        do {
+            var length = readDWord(bytes, off);
+            var type = readString(bytes, off + 4, 4);
+            var res = callback(type, bytes, off, length);
+            off += 12 + length;
+        } while(res !== false && type != "IEND" && off < bytes.length);
+    };
+
+    global.parseAPNG = function(bytes) {
+        var d = new D();
+
+        for (var i = 0; i < PNG_SIGNATURE_BYTES.length; i++) {
+            if (PNG_SIGNATURE_BYTES[i] != bytes[i]) {
+                d.reject("Invalid PNG file signature");
+                return d.promise();
+            }
+        }
+
+        // быстрая проверка анимированности
+        var isAnimated = false;
+        parseChunks(bytes, function(type) {
+            if (type == "acTL") {
+                isAnimated = true;
+                return false;
+            }
+        });
+        if (!isAnimated) {
+            d.reject("Not an animated PNG");
+            return d.promise();
+        }
+
+        var
+                preDataBuilder = new BlobBuilder(),
+                postDataBuilder = new BlobBuilder(),
+                headerDataBytes = null,
+                frame = null,
+                aPNG = {
+                    width:  0,
+                    height: 0,
+                    numPlays:   0,
+                    playTime:   0,
+                    frames:     []
+                };
+
+        parseChunks(bytes, function(type, bytes, off, length) {
+            switch (type) {
+                case "IHDR":
+                    headerDataBytes = bytes.subarray(off + 8, off + 8 + length);
+                    aPNG.width = readDWord(bytes, off + 8);
+                    aPNG.height = readDWord(bytes, off + 12);
+                    break;
+                case "acTL":
+                    aPNG.numPlays = readDWord(bytes, off + 8 + 4);
+                    break;
+                case "fcTL":
+                    if (frame) aPNG.frames.push(frame);
+                    frame = {};
+                    frame.width     = readDWord(bytes, off + 8 + 4);
+                    frame.height    = readDWord(bytes, off + 8 + 8);
+                    frame.left      = readDWord(bytes, off + 8 + 12);
+                    frame.top       = readDWord(bytes, off + 8 + 16);
+                    var delayN      = readWord(bytes, off + 8 + 20);
+                    var delayD      = readWord(bytes, off + 8 + 22);
+                    if (delayD == 0) delayD = 100;
+                    frame.delay = 1000 * delayN / delayD;
+                    aPNG.playTime += frame.delay;
+                    frame.disposeOp = readByte(bytes, off + 8 + 24);
+                    frame.blendOp   = readByte(bytes, off + 8 + 25);
+                    frame.dataParts = [];
+                    break;
+                case "fdAT":
+                    if (frame) frame.dataParts.push(bytes.subarray(off + 8 + 4, off + 8 + length));
+                    break;
+                case "IDAT":
+                    if (frame) frame.dataParts.push(bytes.subarray(off + 8, off + 8 + length));
+                    break;
+                case "IEND":
+                    postDataBuilder.append(subBuffer(bytes, off, 12 + length));
+                    break;
+                default:
+                    preDataBuilder.append(subBuffer(bytes, off, 12 + length));
+            }
+        });
+        if (frame) aPNG.frames.push(frame);
+
+        if (!aPNG.frames.length) {
+            d.reject("Not an animated PNG");
+            return d.promise();
+        }
+
+        // Собираем кадры
+        var createdUrls = 0;
+        for (var f = 0; f < aPNG.frames.length; f++) {
+            frame = aPNG.frames[f];
+
+            var bb = new BlobBuilder();
+            bb.append(PNG_SIGNATURE_BYTES.buffer);
+            headerDataBytes.set(makeDWordArray(frame.width), 0);
+            headerDataBytes.set(makeDWordArray(frame.height), 4);
+            bb.append(makeChunkBytes("IHDR", headerDataBytes).buffer);
+            bb.append(preDataBuilder.getBlob());
+            for (var j = 0; j < frame.dataParts.length; j++)
+                bb.append(makeChunkBytes("IDAT", frame.dataParts[j]).buffer);
+            bb.append(postDataBuilder.getBlob());
+            //frame.url = URL.createObjectURL(bb.getBlob("image/png"));
+            var reader = new FileReader();
+            (function(frame) {
+                reader.onload = function(e) {
+                    frame.url = e.target.result;
+                    createdUrls++;
+                    if (createdUrls == aPNG.frames.length) d.resolve(aPNG);
+                };
+            })(frame);
+            reader.readAsDataURL(bb.getBlob("image/png"));
+            delete frame.dataParts;
+            bb = null;
+        }
+        return d.promise();
+    };
+
+})();
---
+++ content-script.js
@@ -0,0 +1,194 @@
+(function() {
+    var global = (function() {
+        return this;
+    })(), D = Deferred;
+
+    var isEnabled = false;
+
+    chrome.extension.onRequest.addListener(function(request, sender, callback) {
+        if (request.action == "getInfo") {
+            callback({
+                "hostname": global.location.hostname,
+                "enabled":  isEnabled
+            });
+        }
+    });
+
+    chrome.extension.sendRequest({"action": "checkHostname", "data": global.location.hostname},
+            function(response) {
+                if (!response) return;
+
+                isEnabled = true;
+                var allAnimations = [];
+                var urlPromises = {};
+
+                var animateImage = function(image) {
+                    if (typeof urlPromises[image.src] == "undefined") {
+                        urlPromises[image.src] = D.pipeline(image.src)(
+                                loadAndParse,
+                                function(apng) {
+                                    var a = new Animation(apng);
+                                    allAnimations.push(a);
+                                    animationFound();
+                                    return a;
+                                }
+                        );
+                    }
+
+                    urlPromises[image.src].done(function(a) {
+                        var ctxName = a.getCSSCanvasContext();
+                        image.setAttribute("data-apng-src", image.src);
+                        if (!image.hasAttribute("width") && !image.style.width)
+                            image.style.width = global.getComputedStyle(image).width;
+                        if (!image.hasAttribute("height") && !image.style.height)
+                            image.style.height = global.getComputedStyle(image).height;
+                        image.src = chrome.extension.getURL("img/1px.gif");
+                        image.style.backgroundImage = "-webkit-canvas(" + ctxName + ")";
+                        image.style.backgroundSize = "100% 100%";
+                    });
+
+                    return urlPromises[image.src];
+                };
+
+                var ctxNamePrefix = "apng-chrome-ext-css-", ctxNameCounter = 1;
+
+                var Animation = function(apng) {
+                    var thisAnimation = this;
+
+                    this.isActive = false;
+                    this.nextRenderTime = 0;
+
+                    for (var k in apng) this[k] = apng[k];
+
+                    var readyD = new D();
+                    readyD.promise(this);
+
+                    // создаём картинки
+                    var loadedImages = 0, frame;
+                    for (var f = 0; f < this.frames.length; f++) {
+                        var img = new Image();
+                        frame = this.frames[f];
+                        frame.img = img;
+                        img.onload = function() {
+                            loadedImages++;
+                            if (loadedImages == thisAnimation.frames.length) readyD.resolve(thisAnimation);
+                        };
+                        img.onerror = function() {
+                            readyD.reject("Image creation error");
+                        };
+                        img.src = frame.url;
+                        delete frame.url;
+                    }
+
+                    var context = null;
+                    var contextName = null;
+                    this.getCSSCanvasContext = function() {
+                        if (!context) {
+                            contextName = ctxNamePrefix + (ctxNameCounter++);
+                            context = document.getCSSCanvasContext("2d", contextName, this.width, this.height);
+                            this.isActive = true;
+                        }
+                        return contextName;
+                    };
+
+                    var fNum = 0;
+                    var prevF = null;
+
+                    this.renderFrame = function(now) {
+                        if (!context) return;
+                        var f = fNum++ % this.frames.length;
+                        var frame = this.frames[f];
+
+                        if (f == 0) {
+                            context.clearRect(0, 0, this.width, this.height);
+                            prevF = null;
+                            if (frame.disposeOp == 2) frame.disposeOp = 1;
+                        }
+
+                        if (prevF && prevF.disposeOp == 1) {
+                            context.clearRect(prevF.left, prevF.top, prevF.width, prevF.height);
+                        } else if (prevF && prevF.disposeOp == 2) {
+                            context.putImageData(prevF.iData, prevF.left, prevF.top);
+                        }
+                        prevF = frame;
+                        prevF.iData = null;
+                        if (prevF.disposeOp == 2) {
+                            prevF.iData = context.getImageData(frame.left, frame.top, frame.width, frame.height);
+                        }
+                        if (frame.blendOp == 0) context.clearRect(frame.left, frame.top, frame.width, frame.height);
+                        context.drawImage(frame.img, frame.left, frame.top);
+
+                        if (this.numPlays == 0 || fNum / this.frames.length < this.numPlays) {
+                            if (this.nextRenderTime == 0) this.nextRenderTime = now;
+                            while (now > this.nextRenderTime + this.playTime) this.nextRenderTime += this.playTime;
+                            this.nextRenderTime += frame.delay;
+                        } else {
+                            this.isActive = false;
+                        }
+                    };
+                };
+
+                var loadAndParse = function(url) {
+                    var d = new D();
+                    // посылаем сообщение фоновой странице
+                    chrome.extension.sendRequest({"action": "loadAndParse", "data": url}, function(response) {
+                        if (response.ok) {
+                            d.resolve(response.apng);
+                        } else {
+                            d.reject(response.reason);
+                        }
+                    });
+                    return d.promise();
+                };
+
+                var checkImages = function() {
+                    var allImages = document.images; //.querySelectorAll("img[src*='.png']:not([data-apng-status])");
+                    for (var i = 0, l = allImages.length; i < l; i++) {
+                        var image = allImages[i];
+                        if (
+                                image.apngStatus
+                                ||
+                                !/\.png($|\?)/.test(image.src)
+                                &&
+                                !/attachment\.php\?attachmentid=/.test(image.src)
+                        ) continue;
+                        image.apngStatus = "progress";
+                        (function(image) {
+                            animateImage(image).done(function() {
+                                image.apngStatus = "done";
+                            }).fail(function() {
+                                image.apngStatus = "failed";
+                            });
+                        })(image);
+                    }
+                };
+
+                setInterval(checkImages, 1000);
+                checkImages();
+
+                var requestAnimationFrame = global.requestAnimationFrame || global.webkitRequestAnimationFrame;
+
+                // Main animation loop
+                var animationTick = function(t) {
+                    for (var i = 0; i < allAnimations.length; i++) {
+                        var a = allAnimations[i];
+                        while (a.isActive && a.nextRenderTime <= t) a.renderFrame(t);
+                    }
+                    requestAnimationFrame(animationTick);
+                };
+
+                var isAnimationFound = false;
+                var animationFound = function() {
+                    if (!isAnimationFound) {
+                        isAnimationFound = true;
+                        requestAnimationFrame(animationTick);
+                    }
+                    chrome.extension.sendRequest({
+                        "action": "apngFound",
+                        "data": allAnimations.length
+                    });
+                };
+
+            });
+})();
+
