d317ed752074965989be841d281fad07ac0048b5
David <david@hiero.ru>
2014-01-12 00:19:40 +0400

    Перекрытие стиля в ЖЖ: .b-singlepost-body IMG:not([width]):not([height]):not(.i-ljpoll-results-bar) {     width: auto !important;     height: auto !important;     max-width: 100%; }

--- content.js
+++ content.js
@@ -141,8 +141,8 @@
         var image = this, w = image.width, h = image.height;
         loadAndParseURL(image.src)
                 .done(function (a) {
-                    if (!image.hasAttribute("width") && (!image.style.width || image.style.width == "auto")) image.style.width = w + "px";
-                    if (!image.hasAttribute("height") && (!image.style.height || image.style.height == "auto")) image.style.height = h + "px";
+                    if (!image.hasAttribute("width") && (!image.style.width || image.style.width == "auto")) image.style.width = w + "px !important";
+                    if (!image.hasAttribute("height") && (!image.style.height || image.style.height == "auto")) image.style.height = h + "px !important";
                     var ctxName = a.getCanvasName();
                     image.apngContextName = ctxName;
                     image.style.content = "-webkit-canvas(" + ctxName + ")";
